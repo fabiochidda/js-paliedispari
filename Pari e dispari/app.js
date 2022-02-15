6625b5a0e5403265046b472c78a5aa561a3ba485
fabiochidda <fabio.chidda99@gmail.com>
2022-02-15 15:59:04 +0100

    Aggiunto controllo per il numero inserito e la funzione per generare un numero random

--- Pari e dispari/app.js
+++ Pari e dispari/app.js
@@ -3,3 +3,19 @@ console.log(userInputEvenOdd);
 
 let userInputNumber = prompt("Scegli un numero da 1 a 5");
 console.log(userInputNumber);
+
+if (userInputNumber > 5) {
+
+    alert("Per favore inserisci un numero da 1 a 5")
+
+}
+
+function randomNumber(min, max) {
+
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+
+}
+
+console.log(randomNumber(1, 5))
